3a7eee9e77f1dea73a3b00bc38d872e23564d06b
virkumar <virkumar@microsoft.com>
2022-01-16 12:30:44 +0530

    rest api

---
+++ src/main/java/com/example/SpringBootRest/SpringBootRestApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringBootRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+
+}
